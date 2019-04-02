de97a3e3af853c7b2b4a3ef00e1e6a49d3f0742a
emma lapalm <lillypadcrafts@gmail.com>
2019-04-02 06:20:55 -0700

    2 APIs added. waiting on BIT,

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log("this is loaded, yo!")
+
+exports.spotify = {
+    id: ProcessingInstruction.env797a4fc427e949b2964d1107749ea9c3,
+    secret: ProcessingInstruction.env762fc0ccb17a4731af7afd230d447826,
+};
+
+var keys = require("./keys.js");
+
