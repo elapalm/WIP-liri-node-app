9d20914b9d09e06c67018615520fefde2e20cec5
emma lapalm <lillypadcrafts@gmail.com>
2019-04-04 20:10:39 -0700

    ......

--- liri.js
+++ liri.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-
 var spotify = new Spotify(keys.spotify);
-
+var bandsintown = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp",
